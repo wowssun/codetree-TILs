d16e567a65e3bf04c05648b712482d216377307d
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-04 22:52:48 +0900

    Title: 데이터 교환, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

--- 240304/데이터 교환/exchange-data.java
+++ 240304/데이터 교환/exchange-data.java
@@ -3,14 +3,13 @@ public class Main {
        int a = 5;
        int b = 6;
        int c = 7;
-       int temp1, temp2;
+       int temp;
 
-       temp1 = b;
-       b = a;
-       temp2 = c;
-       c = temp1;
-       a = temp2;
-
+      temp = a;
+      a = c;
+      c = b;
+      b = temp;
+    
        System.out.println(a);
        System.out.println(b);
        System.out.println(c);
