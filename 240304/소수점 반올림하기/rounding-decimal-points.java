c4c1a048068d938a14a10957738f8c2839028f79
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-04 22:24:00 +0900

    Title: 소수점 반올림하기, Time: 110ms, Memory: 9MB, Status: Passed - CodeTree

--- 240304/소수점 반올림하기/rounding-decimal-points.java
+++ 240304/소수점 반올림하기/rounding-decimal-points.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
        double a = 25.352;
 
+     // 소수점 첫째 자리로 맞춰서 출력하면 반올림 자동으로 됨.
        System.out.printf("%.1f", a);
     }
 }
