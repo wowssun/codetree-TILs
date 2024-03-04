7be5febee10f086ab54f7fd42f667af85e26ba9f
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-04 23:04:17 +0900

    Title: 점수 입출력, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240304/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int score = sc.nextInt();
+
+        System.out.print("Your score is " + score + " point.");
+    }
+}
