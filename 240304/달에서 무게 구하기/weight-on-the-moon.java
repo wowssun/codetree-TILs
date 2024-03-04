1c12bdc46d4fce2c69dd295340ba43bcf2b00151
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-04 22:19:40 +0900

    Title: 달에서 무게 구하기, Time: 127ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240304/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int weight = 13;
+        double ratio = 0.165;
+
+        System.out.printf("%d * %.6f = %.6f",weight, ratio, (weight * ratio));
+    }
+}
