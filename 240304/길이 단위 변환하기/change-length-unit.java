478ae08842853605ac09820f29c4fa5e0e6d9e6e
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-04 22:28:01 +0900

    Title: 길이 단위 변환하기, Time: 95ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240304/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double ftCm = 30.48;
+        double ftMi = 160934;
+
+        System.out.printf("9.2ft = %.1fcm\n", (9.2 * ftCm));
+        System.out.printf("1.3mi = %.1fcm\n", (1.3 * ftMi));
+    }
+}
