cdcd86c47752c6db26cd7e4831598d38f0a2f2b7
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-05 17:18:30 +0900

    Title: 합과 평균, Time: 181ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240305/합과 평균/sum-and-avg.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = a + b;
+        double avg = (a + b) / 2.0;
+
+        System.out.println(sum + " " + avg);
+
+
+    }
+}
