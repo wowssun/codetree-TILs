2107ba9cb5cdb00b0bb462671ed774f4dc8e7e50
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-05 16:12:24 +0900

    Title: 1시간 뒤 시간 출력, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240305/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.useDelimiter(":");
+
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+
+        h += 1;
+
+        System.out.print(h + ":" + m);
+    }
+}
