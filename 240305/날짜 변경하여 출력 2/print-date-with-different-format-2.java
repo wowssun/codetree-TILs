1509c5a3e093a9e44a48d333b96512a7efdff6db
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-05 16:16:12 +0900

    Title: 날짜 변경하여 출력 2, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240305/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+       sc.useDelimiter("-");
+
+       int month = sc.nextInt();
+       int day = sc.nextInt();
+       int year = sc.nextInt();
+
+       System.out.printf("%d.%d.%d", year, month, day);
+    }
+}
