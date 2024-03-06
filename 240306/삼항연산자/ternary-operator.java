d73a38b3a413c286831ef71f850abb09f62a7679
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-06 23:15:59 +0900

    Title: 삼항연산자, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240306/삼항연산자/ternary-operator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int score = sc.nextInt();
+
+        String result = score == 100 ? "pass" : "failure";
+
+        System.out.println(result);
+
+    }
+}
