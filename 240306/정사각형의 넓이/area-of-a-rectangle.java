faf764c21ee081ee899720516c7feb799d864c90
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-06 22:48:03 +0900

    Title: 정사각형의 넓이, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240306/정사각형의 넓이/area-of-a-rectangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int area = n * n;
+
+        System.out.println(area);
+
+        if(n < 5) {
+            System.out.println("tiny");
+        }
+    }
+}
