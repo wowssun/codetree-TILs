9b4f93170238d4c39c7cda945c435ebb0d39d830
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-09 23:32:07 +0900

    Title: 남녀노소 구분짓기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240309/남녀노소 구분짓기/sex-and-age.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int gender = sc.nextInt();
+        int age = sc.nextInt();
+
+        if (gender == 1) {
+            if (age >= 19) {
+                System.out.println("WOMAN");
+            }else {
+                System.out.println("GIRL");
+            }
+        } else {
+            if (age >= 19) {
+                System.out.println("MAN");
+            }else {
+                System.out.println("BOY");
+            }
+        }
+    }
+}
