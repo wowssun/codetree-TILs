89594927b0090728402ae4b89cb9126cf5afd344
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-09 23:27:59 +0900

    Title: 두 사람, Time: 114ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240309/두 사람/two-person.java
+++ 240309/두 사람/two-person.java
@@ -10,7 +10,7 @@ public class Main {
         int h2Age = sc.nextInt();
         String h2Gen = sc.next();
 
-        if ((h1Age >= 19 || h2Age >= 19) && (h1Gen.equals("M") || h2Gen.equals("M"))){
+        if ((h1Age >= 19 && h1Gen.equals("M") ) || (h1Age >= 19 && h2Gen.equals("M"))){
             System.out.println(1);
         } else {
             System.out.println(0);
