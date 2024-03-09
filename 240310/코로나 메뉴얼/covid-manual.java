2395cb18903f3f76c81c346ad2ad91491f9b4d9d
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-10 01:12:32 +0900

    Title: 코로나 메뉴얼, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240310/코로나 메뉴얼/covid-manual.java
+++ 240310/코로나 메뉴얼/covid-manual.java
@@ -14,9 +14,9 @@ public class Main {
         int cD = sc.nextInt();
 
        // A가 2명 이상인지 판단하기
-        if(a == 'Y' && aD >= 37) {
+        if(a.equals("Y") && aD >= 37) {
             // 첫 번째 사람이 A라면, 남은 두 사람 중 한 사람이라도 A면 됩니다.
-            if((b == 'Y' && bD >= 37) || (c == 'Y' && cD >= 37)){
+            if((b.equals("Y") && bD >= 37) || (c.equals("Y") && cD >= 37)){
                 System.out.println("E");
             }else {
                 System.out.println("N");
@@ -24,7 +24,7 @@ public class Main {
         }
         else {
             // 첫 번째 사람이 A가 아니라면, 남은 두 사람 모두 A여야만 합니다.
-            if((b == 'Y' && bD >= 37) && (c == 'Y' && cD >= 37)) {
+            if((b.equals("Y") && bD >= 37) && (c.equals("Y") && cD >= 37)) {
                 System.out.println("E");
             }else {
                 System.out.println("N");
