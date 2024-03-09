8e6d5c3e77d1cc1288ea71dd0f8ab107f90962db
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-10 01:11:11 +0900

    Title: 코로나 메뉴얼, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240310/코로나 메뉴얼/covid-manual.java
+++ 240310/코로나 메뉴얼/covid-manual.java
@@ -13,62 +13,22 @@ public class Main {
         String c = sc.next();
         int cD = sc.nextInt();
 
-        int count = 0;
-        String grade;
-
-        if (a.equals("Y")) {
-            if (aD >= 37) {
-                grade = "A";  
-                count ++;  
-            } else {
-                grade = "C"; 
-            }
-            
-        } else {
-             if (aD >= 37) {
-                grade = "B";    
-            } else {
-                grade = "D"; 
+       // A가 2명 이상인지 판단하기
+        if(a == 'Y' && aD >= 37) {
+            // 첫 번째 사람이 A라면, 남은 두 사람 중 한 사람이라도 A면 됩니다.
+            if((b == 'Y' && bD >= 37) || (c == 'Y' && cD >= 37)){
+                System.out.println("E");
+            }else {
+                System.out.println("N");
             }
         }
-
-        if (b.equals("Y")) {
-            if (bD >= 37) {
-                grade = "A";  
-                count ++;  
-            } else {
-                grade = "C"; 
-            }
-            
-        } else {
-             if (bD >= 37) {
-                grade = "B";    
-            } else {
-                grade = "D"; 
+        else {
+            // 첫 번째 사람이 A가 아니라면, 남은 두 사람 모두 A여야만 합니다.
+            if((b == 'Y' && bD >= 37) && (c == 'Y' && cD >= 37)) {
+                System.out.println("E");
+            }else {
+                System.out.println("N");
             }
         }
-
-        if (c.equals("Y")) {
-            if (cD >= 37) {
-                grade = "A";  
-                count ++;  
-            } else {
-                grade = "C"; 
-            }
-            
-        } else {
-             if (cD >= 37) {
-                grade = "B";    
-            } else {
-                grade = "D"; 
-            }
-        }
-
-        if (count >= 2){
-            System.out.println("E");
-        } else {
-            System.out.println("N");
-        }
-
     }
 }
