e5d0c0cf871386fc405cb8a9685ae1d40e11e293
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-09 23:03:36 +0900

    Title: 숫자의 계절은, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 240309/숫자의 계절은/season-of-num.java
+++ 240309/숫자의 계절은/season-of-num.java
@@ -6,14 +6,13 @@ public class Main {
 
         int m = sc.nextInt();
         
-        if (3 <= m && m <= 5){
+        if(m >= 12 || m <= 2)
+            System.out.println("Winter");
+        else if(m <= 5)
             System.out.println("Spring");
-        } else if (6 <= m && m <= 8) {
+        else if(m <= 8)
             System.out.println("Summer");
-        } else if (9 <= m && m <= 11) {
+        else
             System.out.println("Fall");
-        } else if (m == 12 || m == 1 || m == 2) {
-             System.out.println("Winter");
-        }
     }
 }
