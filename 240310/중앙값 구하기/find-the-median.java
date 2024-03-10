1469e2dcdbc825157b7501277457f830e407df2f
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-10 22:16:06 +0900

    Title: 중앙값 구하기, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240310/중앙값 구하기/find-the-median.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a >= b) {
+            if (c >= a) {
+                System.out.println(a);
+            } else {
+                if (b >= c) {
+                    System.out.println(b);
+                } else {
+                    System.out.println(c);
+                }
+                
+            }
+        } else {
+            if (c >= b) {
+                System.out.println(b);
+            } else {
+                if (a >= c) {
+                System.out.println(a);
+                }else {
+                  System.out.println(c);  
+                }
+                
+            }
+        }
+    }
+}
