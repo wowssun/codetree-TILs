58e24761b8073fc5de69dcdd7b823fb68e7358b4
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-10 22:23:23 +0900

    Title: 중앙값 구하기, Time: 225ms, Memory: 10MB, Status: Passed - CodeTree

--- 240310/중앙값 구하기/find-the-median.java
+++ 240310/중앙값 구하기/find-the-median.java
@@ -8,27 +8,31 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        if (a >= b) {
-            if (c >= a) {
+        // 그림 그려서 생각하기 
+        if(a > b) {
+            if(c > a) {
+                // a > b, c > a 일때 (c > a > b)
                 System.out.println(a);
-            } else {
-                if (b >= c) {
-                    System.out.println(b);
-                } else {
-                    System.out.println(c);
-                }
-                
             }
-        } else {
-            if (c >= b) {
+            // a > b, a > c 일때 (a가 가장 크고, b와 c중 더 큰 수가 중앙값)
+            else if(b > c) {
                 System.out.println(b);
-            } else {
-                if (a >= c) {
+            }
+            else {
+                System.out.println(c);
+            }
+        }
+        else {
+            if(c > b) {
+                // b > a, c > b 일때 (c > b > a)
+                System.out.println(b);
+            }
+            // b > a, b > c 일때 (b가 가장 크고, a와 c중 더 큰 수가 중앙값)
+            else if(a > c) {
                 System.out.println(a);
-                }else {
-                  System.out.println(c);  
-                }
-                
+            }
+            else {
+                System.out.println(c);
             }
         }
     }
