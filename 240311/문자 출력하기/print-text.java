a2b0b81b2378e14df08481f4d2c845088efdf7b5
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-11 23:17:55 +0900

    Title: 문자 출력하기, Time: 112ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240311/문자 출력하기/print-text.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        for (int i = 1; i <= 8; i++) {
+            System.out.print(str);
+        }
+    }
+}
