4e64e3abd053b92988908d9f0c554efa1665295a
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-11 23:03:12 +0900

    Title: 3의 배수 출력하기, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240311/3의 배수 출력하기/print-multiple-of-number-three.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int i = 1;
+
+        while (i <= n) {
+            if (i % 3 == 0) {
+                System.out.print(i + " ");
+                i++;
+            }
+            i++;
+        }
+    }
+}
