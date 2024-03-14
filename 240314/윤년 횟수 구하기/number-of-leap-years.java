79be9fe7702c906c54954846483dc56459243cd1
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-14 23:49:13 +0900

    Title: 윤년 횟수 구하기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240314/윤년 횟수 구하기/number-of-leap-years.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if ((i % 4 == 0 && i % 100 != 0) || i % 400 == 0) {
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+
+    }
+}
