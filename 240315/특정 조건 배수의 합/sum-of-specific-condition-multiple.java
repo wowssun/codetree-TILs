f0a23147e68233b8620b2c00e5e85e48018fdba7
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-15 19:38:25 +0900

    Title: 특정 조건 배수의 합, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

--- 240315/특정 조건 배수의 합/sum-of-specific-condition-multiple.java
+++ 240315/특정 조건 배수의 합/sum-of-specific-condition-multiple.java
@@ -8,11 +8,23 @@ public class Main {
         int b = sc.nextInt();
         int sum = 0;
 
-        for (int i = a; i <= b; i++) {
-            if (i % 5 == 0) {
-                sum += i;
+
+        if (b >= a) {
+            for (int i = a; i <= b; i++) {
+                if (i % 5 == 0) {
+                    sum += i;
+                }
             }
+
+        } else {
+            for (int i = b; i <= a; i++) {
+                if (i % 5 == 0) {
+                    sum += i;
+                }
+            }
+
         }
+        
         System.out.print(sum);
 
     }
