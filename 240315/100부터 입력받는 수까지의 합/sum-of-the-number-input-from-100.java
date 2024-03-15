837a2c157032d413e54e7cfdc83a85288b7fe778
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-15 19:08:04 +0900

    Title: 100부터 입력받는 수까지의 합, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240315/100부터 입력받는 수까지의 합/sum-of-the-number-input-from-100.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+       int n = sc.nextInt();
+       int sum = 0;
+
+       for (int i = n; i <= 100; i++) {
+            sum += i;
+       }
+       System.out.print(sum);
+    }
+}
