a0c74bd708ebbdbc1360cdf202c921ba16e0d0c8
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-16 19:59:32 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240316/20대의 평균나이/average-age-of-20.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0, cnt = 0;
+
+        while (true) {
+            int age = sc.nextInt();
+
+            if (age >= 30) {
+                break;
+            }
+            sum += age;
+            cnt++;
+        }
+        double avg = (double)sum / cnt;
+        System.out.printf("%.2f", avg);
+    }
+}
