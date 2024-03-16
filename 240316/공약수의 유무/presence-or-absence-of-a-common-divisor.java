b828a416f3e6d38a4779a529ec0ef232e00938fc
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-16 21:32:28 +0900

    Title: 공약수의 유무, Time: 152ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240316/공약수의 유무/presence-or-absence-of-a-common-divisor.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        boolean state = false;
+
+        for (int i = a; i <= b; i++) {
+            if (1920 % i == 0 || 2880 % i == 0) {
+                state = true;
+            }
+        }
+
+        if (state) {
+            System.out.print(1);
+        } else {
+            System.out.print(0);
+        }
+    }
+}
