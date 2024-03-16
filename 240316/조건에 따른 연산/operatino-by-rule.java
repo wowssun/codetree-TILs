f3bcbd9e547387a1eeb96c7a631f809f34d601e6
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-16 20:59:06 +0900

    Title: 조건에 따른 연산, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240316/조건에 따른 연산/operatino-by-rule.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        while (true) {
+            if (n >= 1000) {
+                System.out.print(cnt);
+                break;
+            } 
+
+            if (n % 2 == 0) {
+                n = n * 3 + 1;
+            } else {
+                n = n * 2 + 2;
+            }
+            cnt++;
+        }
+
+    }
+}
