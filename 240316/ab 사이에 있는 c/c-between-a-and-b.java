deeee58ff0e9f258f6e716a5ff312b27603598a3
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-16 21:05:35 +0900

    Title: ab 사이에 있는 c, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240316/ab 사이에 있는 c/c-between-a-and-b.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        boolean state = false;
+
+        for (int i = a; i <= b; i++) {
+            if (i % c == 0) {
+                state = true;
+            }
+        }
+
+        if (state) {
+            System.out.print("YES");
+        } else {
+            System.out.print("NO");
+        }
+    }
+}
