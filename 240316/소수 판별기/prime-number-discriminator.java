d1acf38df968be6e6902b733fa20eb5b3091dbb1
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-16 21:38:03 +0900

    Title: 소수 판별기, Time: 214ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240316/소수 판별기/prime-number-discriminator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        boolean result = true;
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                result = false;
+            }
+        }
+
+        if (result) {
+            System.out.print("P");
+        } else {
+            System.out.print("C");
+        }
+    }
+}
