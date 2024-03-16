57bb043904c7a25d68d62c925f852125590f78ef
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-16 21:23:35 +0900

    Title: 합성수 판별기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240316/합성수 판별기/factorization-discriminator.java
+++ 240316/합성수 판별기/factorization-discriminator.java
@@ -7,7 +7,7 @@ public class Main {
         int n = sc.nextInt();
         int cnt = 0;
 
-        for(int i = 1; i <= n; i++) {
+        for(int i = 2; i <= n; i++) {
             if (n % i == 0) {
                 cnt++;
             }
