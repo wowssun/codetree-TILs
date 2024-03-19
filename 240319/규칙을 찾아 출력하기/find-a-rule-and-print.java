9f5d8f2dff5cb2a978c65c55398e05b6d42eb859
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-19 16:12:12 +0900

    Title: 규칙을 찾아 출력하기, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240319/규칙을 찾아 출력하기/find-a-rule-and-print.java
+++ 240319/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -8,7 +8,7 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == 0 || i == 4 || j == 0 || j == 4) {
+                if (i == 0 || i == n-1 || j == 0 || j == n-1) {
                     System.out.print("* ");
                 } else if (i > j) {
                     System.out.print("* ");
