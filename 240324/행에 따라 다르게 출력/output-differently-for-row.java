1fa7c153684a69accdee541bef2d45ae933b4ab9
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-24 12:44:47 +0900

    Title: 행에 따라 다르게 출력, Time: 250ms, Memory: 12MB, Status: Passed - CodeTree

--- 240324/행에 따라 다르게 출력/output-differently-for-row.java
+++ 240324/행에 따라 다르게 출력/output-differently-for-row.java
@@ -6,25 +6,16 @@ public class Main {
 
         int n = sc.nextInt();
 
-        int cnt = 1;
+        int cnt = 0;
 
         for(int i = 0; i < n; i++) {
-            if(i % 2 == 0) { // 0행부터 시작 2, 4...
-                for(int j = 0; j < n; j++) {
-                    System.out.print(cnt + " ");
-                    cnt++; //  1씩 증가
-                }
-            } else { // 1행부터 시작 3,5...
-                cnt += 1; // 짝수 행의 시작 전에 cnt를 조정 
-                for(int j = 0; j < n; j++) {
-                    System.out.print(cnt + " ");
-                    if(j == (n-1)){
-                        cnt += 1;
-                    } else {
-                         cnt += 2; //  cnt가 2씩 증가
-                    }
-                   
-                }
+            for(int j = 0; j < n; j++) {
+                if(i % 2 == 0)   //  if로 먼저 판단 후에 
+                    cnt++;
+                else
+                    cnt += 2;
+                
+                System.out.print(cnt + " ");  // 출력
             }
             System.out.println();
         }
