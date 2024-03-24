4cea1a40ff913c8dd49ac592784b0dbfcc258dcf
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-24 17:24:33 +0900

    Title: 구구단 만들기, Time: 168ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240324/구구단 만들기/print-multiplication-table.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = 1; i <= 9; i++) {
+            for (int j = b; j >= a ; j--) {
+                if (j % 2 == 0) {
+                    System.out.print(j + " * " + i + " = " + (j * i));
+
+                    if (j > a){
+                    System.out.print(" / ");
+                    }
+                }
+              
+            }
+            System.out.println();
+        }
+
+    }
+}
