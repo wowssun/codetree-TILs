edc3841b6a6ae7ac7f70793e3055bb3243179cea
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-03-28 23:32:22 +0900

    Title: 알파벳 출력하기 2, Time: 108ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240328/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        char a = 'A';
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i*2; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = n-i; j > 0; j--) {
+                System.out.print(a + " ");
+                a++;
+            }
+            System.out.println();
+        }
+    }
+}
