068ce1f9214247815a31cc48b67cf6726e9da4c7
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-04-02 23:06:02 +0900

    Title: 특정 숫자 도달하기, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240402/특정 숫자 도달하기/reaching-specific-number.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int [] arr = new int[10];
+        int sum = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+
+            if (arr[i] >= 250) {
+                break;
+            }
+            cnt++;
+            sum += arr[i];
+            
+            if (cnt == 10) {
+                break;
+            }         
+        }
+        double avg = (double)sum / cnt;
+        System.out.printf("%d %.1f",sum,avg);
+    }
+}
