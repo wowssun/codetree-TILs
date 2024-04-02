e26f52630449b6b7baf3b165d7de11f8b7c6572f
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-04-02 21:54:07 +0900

    Title: 소수 구하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240402/소수 구하기/get-prime.java
+++ 240402/소수 구하기/get-prime.java
@@ -20,5 +20,20 @@ public class Main {
            }
                
        }
+
+     /*   for(int i = 1; i <= n; i++) {
+            if(i == 1) continue;
+            boolean isprime = true;
+            
+            for(int j = 2; j < i; j++)
+                if(i % j == 0) isprime = false;
+            
+            if(isprime)
+                System.out.print(i + " ");
+        }
+
+    */
+
+
     }
 }
