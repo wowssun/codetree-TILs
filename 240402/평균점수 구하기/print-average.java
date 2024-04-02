c88e2acbb623fd6478bdd754bbd5db8cdbe127ed
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-04-02 23:20:24 +0900

    Title: 평균점수 구하기, Time: 173ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240402/평균점수 구하기/print-average.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+       double [] score = new double[8];
+       double sum = 0;
+
+       for (int i = 0; i < 8; i++) {
+         score[i] = sc.nextDouble();
+         sum += score[i];
+       }
+       
+       double avg = sum / 8;
+       System.out.printf("%.1f", avg);
+    }
+}
