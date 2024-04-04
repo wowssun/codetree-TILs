fb13bd59dd612d5ded41cc4f5f70d2e39e5d377a
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-04-04 20:30:46 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240404/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
+++ 240404/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
@@ -6,6 +6,7 @@ public class Main {
 
         int [] arr = new int[100];
         int sum = 0;
+        int cnt = 0;
 
         for (int i = 0; i < 100; i++) {
             arr[i] = sc.nextInt();
@@ -13,8 +14,11 @@ public class Main {
             if(arr[i] == 0){
                 break;
             }
+            cnt++;
+        }
 
-            sum += arr[i];
+        for (int j = cnt -3; j < cnt; j++){
+            sum += arr[j];
         }
         System.out.print(sum);
     }
