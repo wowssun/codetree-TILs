9b17f90644e74ecd7dd4493ccf1ce43533549a6f
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-04-09 21:40:10 +0900

    Title: 나눗셈의 나머지, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 240409/나눗셈의 나머지/remainder-of-division.java
+++ 240409/나눗셈의 나머지/remainder-of-division.java
@@ -6,26 +6,19 @@ public class Main {
 
         int a = sc.nextInt();
         int b = sc.nextInt();
-
         int [] cnt = new int[10];
-
-        int i = 0;
-
-        while (true) {
-
-            cnt[a % b]++; 
-            a = a / b;
-
-            if (a <= 1) {
-                break;
-            }
-
-        }
-
         int sum = 0;
-        for (int j = 0; j < 10; j++) {
-            sum += cnt[j] * cnt[j];
+
+        while (a > 1) {
+            cnt[a % b]++; 
+            a /= b;
         }
+        
+        // 나머지는 b 미만으로 나옴 
+        for (int i= 0; i < b; i++) {
+            sum += cnt[i] * cnt[i];
+        }
+
         System.out.print(sum);
     }
 }
