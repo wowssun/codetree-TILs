12245badd354e817564b9938add2bba19a510f2d
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-04-10 13:44:31 +0900

    Title: 개수 세기, Time: 189ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240410/개수 세기/count-numbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int [] arr = new int[n];
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+            if (arr[i] == m) {
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
