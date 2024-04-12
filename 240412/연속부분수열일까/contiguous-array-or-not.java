f590307b5de26f48810914eefb4f1df9d80c64b8
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-04-12 21:15:51 +0900

    Title: 연속부분수열일까, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240412/연속부분수열일까/contiguous-array-or-not.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int [] arr1 = new int[n1];
+        int [] arr2 = new int[n2];
+        boolean result = false;
+    
+        // 수열 a
+        for (int i = 0; i < n1; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        // 수열 b
+        for (int i = 0; i < n2; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        // 수열 b의 0번째 자리와 수열 a의 원소를 순서대로 비교 
+        for (int i = 0; i < n1; i++) {
+            if (arr1[i] == arr2[0]){  
+                for (int j = 0; j < n2; j++) {
+                    if (arr1[i] == arr2[j]){
+                       result = true;
+                       i++; 
+                    } else {
+                        result = false;
+                        break;
+                    }
+                } 
+            }
+        }
+
+        if (result) {
+            System.out.print("Yes");
+        } else {
+            System.out.print("No");       
+        }
+    }
+}
