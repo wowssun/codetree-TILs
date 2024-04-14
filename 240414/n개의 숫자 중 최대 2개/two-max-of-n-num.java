e6e504bc2f30849d4fbeeb49016e0f7f7fef8440
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-04-14 21:50:21 +0900

    Title: n개의 숫자 중 최대 2개, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240414/n개의 숫자 중 최대 2개/two-max-of-n-num.java
+++ 240414/n개의 숫자 중 최대 2개/two-max-of-n-num.java
@@ -26,3 +26,27 @@ public class Main {
         }
     }
 }
+
+/* if (A[0] > A[1]){
+            max1 = A[0];
+            max2 = A[1];
+        }
+        else{
+            max1 = A[1];
+            max2 = A[0];
+        }
+
+        // Step 2: 3번째 원소부터 보면서 max1과 max2를 갱신합니다.
+        for (int i = 2; i < n; i++) {
+            if (A[i] >= max1) {
+                // Case 1: 지금까지 본 숫자들보다 좋다면
+                //         max2, max1 모두 갱신해줍니다.
+                max2 = max1;
+                max1 = A[i];
+            } 
+            else if (A[i] > max2){
+                // Case 2: max2보다만 좋다면 max2를 갱신합니다.
+                max2 = A[i];
+            }
+        }
+*/
