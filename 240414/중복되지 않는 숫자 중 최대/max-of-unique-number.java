4ab882a813bfbc7e50d8c92e85bbfc2797e449b8
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-04-14 22:20:14 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 158ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240414/중복되지 않는 숫자 중 최대/max-of-unique-number.java
+++ 240414/중복되지 않는 숫자 중 최대/max-of-unique-number.java
@@ -11,19 +11,23 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
-        int maxVal = arr[0];
+        int maxVal = -1;  // 우선 max를 =1로 지정 
 
         for (int i = 1; i < n; i++) {
-            if (arr[i] > maxVal){
-                maxVal = arr[i];
+          if (maxVal < arr[i]) {       // -1과 원소를 비교한다. 
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                if (arr[i] == arr[j]) {     // 비교한 원소가 더 큰 경우, 중복 확인 
+                    cnt++;
+                }
             }
+
+            if (cnt == 1) {         // 위에서 구한 max가 중복이 없다면 
+                maxVal = arr[i];    // 최댓값
+            }                       // 만약 중복이 있다면 다시 maxVal = -1인 상태로 다음 원소와 비교 
+          }
         }
 
-        for (int i = 0; i < n; i++) {
-            if (maxVal == arr[i]) {
-                maxVal = -1;
-            }
-        }
         System.out.print(maxVal);
     }
 }
