c0ad1ac67dfb59f9514d1930031d8e9a2ae4dd43
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-04-16 22:59:51 +0900

    Title: 500 근처의 수, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240416/500 근처의 수/near-500.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int [] arr = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int minVal = Integer.MAX_VALUE;
+        int maxVal = Integer.MIN_VALUE;
+
+        for (int i = 0; i < 10; i++) {
+            if (arr[i] > 500 && minVal > arr[i]){ 
+                minVal = arr[i];
+                
+            } else if (arr[i] < 500 && maxVal < arr[i]) {
+                maxVal = arr[i];
+            }
+        }
+        System.out.print(maxVal + " " + minVal);
+
+
+
+
+
+
+
+
+    }
+}
