576a3d799e63d06e8b872c24cc22007d0169407e
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-04-19 23:20:44 +0900

    Title: 두 숫자의 차의 최솟값, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240419/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.java
+++ 240419/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.java
@@ -6,11 +6,12 @@ public class Main {
 
         int n = sc.nextInt();
         int [] arr = new int[n];
-        int gap = Integer.MAX_VALUE;
-
+        
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
+
+        int gap = arr[1] - arr[0];
 
         for (int i = 0; i < n-1; i++) {
             for (int j = i+1; j < n; j++) {
