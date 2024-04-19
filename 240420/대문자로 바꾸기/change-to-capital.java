fc3205c30970074ffe538ff4e2ad01e1895047e5
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-04-20 00:11:18 +0900

    Title: 대문자로 바꾸기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240420/대문자로 바꾸기/change-to-capital.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+       char [][] lower = new char[5][3];
+
+       for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 3; j++) {
+            lower[i][j] = sc.next().charAt(0);
+        }
+       }
+
+       for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 3; j++) {
+            System.out.print((char)(lower[i][j] - 32) + " "); // 32를 빼주고 char으로 형변환 하기 
+        }
+        System.out.println();
+       }
+    }
+}
