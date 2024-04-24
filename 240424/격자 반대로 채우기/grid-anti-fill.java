3af47bac1a37e1819dfadc4ac9b320132473cf50
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-04-24 22:31:57 +0900

    Title: 격자 반대로 채우기, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 240424/격자 반대로 채우기/grid-anti-fill.java
+++ 240424/격자 반대로 채우기/grid-anti-fill.java
@@ -10,32 +10,17 @@ public class Main {
 
         
         for (int j = n-1; j >= 0; j--) {
-            if ((n-1) % 2 == 1) {
-                if (j % 2 == 0) {
-                    for (int i = 0; i < n; i++) {
-                        arr[i][j] = num++;
+            if ((n - 1 -j) % 2 == 0) {
+                for (int i = n-1; i >= 0; i--) {
+                    arr[i][j] = num++;
                     }
-                } else {
-                    for (int i = n-1; i >= 0; i--) {
-                        arr[i][j] = num++;
-                    }
-                }
             } else {
-                if (j % 2 == 1) {
                 for (int i = 0; i < n; i++) {
                     arr[i][j] = num++;
                 }
-            } else {
-                for (int i = n-1; i >= 0; i--) {
-                    arr[i][j] = num++;
-                }
             }
-
-            }
-            
         }
-        
-            
+          
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(arr[i][j] + " ");
