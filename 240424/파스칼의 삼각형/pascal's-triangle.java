2735f97bb0e2b08c502a16a07ad8c697415e1f19
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-04-24 23:04:03 +0900

    Title: 파스칼의 삼각형, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240424/파스칼의 삼각형/pascal's-triangle.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int [][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = 1;
+            arr[i][i] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i][j] > 0) {
+                    System.out.print(arr[i][j] + " ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
