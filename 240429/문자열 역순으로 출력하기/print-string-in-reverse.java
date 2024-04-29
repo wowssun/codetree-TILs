5381099ee75109fd1a5297189190777f916c64a2
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-04-29 21:30:10 +0900

    Title: 문자열 역순으로 출력하기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240429/문자열 역순으로 출력하기/print-string-in-reverse.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+       String [] arr = new String[4];
+
+       for (int i = 0; i < 4; i++) {
+            arr[i] = sc.nextLine(); 
+       }
+
+       for (int i = 3; i >= 0; i--) {
+        System.out.println(arr[i]);
+       }
+    }
+}
