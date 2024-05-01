86def1dc23c4af365de6e1ec2cf4eb1dff48ca3a
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-05-01 22:49:06 +0900

    Title: 두 문자열을 이어붙였을 때, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240501/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        String ab = a + b;
+        String ba = b + a;
+
+        if (ab.equals(ba)) {
+            System.out.print("true");
+        } else {
+            System.out.print("false");
+        }
+
+    }
+}
