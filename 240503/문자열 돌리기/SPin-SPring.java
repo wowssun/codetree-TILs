c81ae1b5c0f5c4f1b00a35c546d90cedb42bfee0
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-05-03 21:45:45 +0900

    Title: 문자열 돌리기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240503/문자열 돌리기/SPin-SPring.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        int len = str.length();
+
+        System.out.println(str);
+
+        for (int i = 0; i < len; i++) {
+            str = str.substring(len - 1, len) + str.substring(0,len - 1);
+            System.out.println(str);
+        }
+    }
+}
