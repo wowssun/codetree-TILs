32b38dd4f1747cb292343a3b549c606e97f99c09
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-05-04 23:10:50 +0900

    Title: 알파벳 지우기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240504/알파벳 지우기/remove-alphabet.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.next();
+        String str2 = sc.next();
+
+        String str1New = "";
+        String str2New = "";
+
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) >= '0' && str1.charAt(i) <= '9') {
+                str1New += str1.charAt(i);
+            }
+        }
+
+        for (int i = 0; i < str2.length(); i++) {
+            if (str2.charAt(i) >= '0' && str2.charAt(i) <= '9') {
+                str2New += str2.charAt(i);
+            }
+        }
+
+        int str1Num = Integer.parseInt(str1New);
+        int str2Num = Integer.parseInt(str2New);
+
+        System.out.print(str1Num + str2Num);
+    }
+}
