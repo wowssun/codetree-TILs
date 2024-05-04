db3ef717247b09b5d71366d0a62da72edaaf8848
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-05-04 18:45:14 +0900

    Title: 그 전 알파벳, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240504/그 전 알파벳/before-the-alphabet.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char n = sc.next().charAt(0);
+
+        if (n == 'a') {
+            System.out.print('z');
+        } else {
+            n--;
+        }
+
+        System.out.print(n);
+    }
+}
