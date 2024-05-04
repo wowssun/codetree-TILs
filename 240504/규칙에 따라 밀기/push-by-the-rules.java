5f925913559288577ce5365c5a8c1299e4852caf
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-05-04 16:54:24 +0900

    Title: 규칙에 따라 밀기, Time: 123ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240504/규칙에 따라 밀기/push-by-the-rules.java
+++ 240504/규칙에 따라 밀기/push-by-the-rules.java
@@ -7,12 +7,9 @@ public class Main {
        String a = sc.next();
        String str = sc.next();
        int lenA = a.length();
-   
-       char [] arr = str.toCharArray();
-       int lenArr = arr.length;
 
-       for (int i = 0; i < lenArr; i++) {
-            if (arr[i] == 'L') {
+       for (int i = 0; i < str.length(); i++) {
+            if (a.charAt(i) == 'L') {
                 a = a.substring(1) + a.substring(0,1);
             } else {
                 a = a.substring(lenA - 1) + a.substring(0, lenA - 1); 
