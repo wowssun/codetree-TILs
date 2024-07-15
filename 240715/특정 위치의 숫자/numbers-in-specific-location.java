64f99581b55e67ec4bf6767c7f099ee6b832786b
wow_dev <119738419+wowssun@users.noreply.github.com>
2024-07-15 22:35:08 +0900

    Title: 특정 위치의 숫자, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240715/특정 위치의 숫자/numbers-in-specific-location.java
+++ 240715/특정 위치의 숫자/numbers-in-specific-location.java
@@ -5,7 +5,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int [] arr = new int[10];
-        int sum = 0;
 
         for (int i = 0; i < 10; i++) {
             arr[i] = sc.nextInt();
