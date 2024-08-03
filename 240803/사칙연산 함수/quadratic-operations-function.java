a612e6e866a925f10f788ee7a122f4398d6f79c8
wowssun <119738419+wowssun@users.noreply.github.com>
2024-08-03 14:00:07 +0900

    Title: 사칙연산 함수, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240803/사칙연산 함수/quadratic-operations-function.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void multiple(int a, int c) {
+        System.out.print(a + " * " + c + " = " + (a*c));
+    }
+    public static void plus(int a, int c) {
+        System.out.print(a + " + " + c + " = " + (a+c));
+    }
+    public static void minus(int a, int c) {
+        System.out.print(a + " - " + c + " = " + (a-c));
+    }
+    public static void division(int a, int c) {
+        System.out.print(a + " / " + c + " = " + (int)(a/c));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        char o = sc.next().charAt(0);
+        int c = sc.nextInt();
+
+        switch(o) {
+            case '*' : multiple(a, c); break;
+            case '+' : plus(a, c); break;
+            case '-' : minus(a, c); break;
+            case '/' : division(a, c); break;
+            default : System.out.print("False");
+        }
+    }
+}
