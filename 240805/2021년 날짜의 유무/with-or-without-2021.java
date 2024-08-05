3256fc34d5d764e739dd0365ca9696c035404a1b
wowssun <119738419+wowssun@users.noreply.github.com>
2024-08-05 20:57:15 +0900

    Title: 2021년 날짜의 유무, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240805/2021년 날짜의 유무/with-or-without-2021.java
+++ 240805/2021년 날짜의 유무/with-or-without-2021.java
@@ -2,26 +2,26 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static boolean exist2021(int m, int d) {
-        if (m < 13) {
-            if (m == 2) {
-                if (d < 29) {
-                    return true;
-                }
-            } else if (m == 4 || m == 6 || m == 9 || m ==11){
-                    if (d < 31) {
-                        return true;
-                    }
-                } else {
-                    if (d < 32) {
-                        return true;
-                    }
-           }
-            
+    public static int lastDay(int m) {
+        if(m == 2) {
+            return 28;
         }
-        
+            
+        if(m == 4 || m == 6 || m == 9 || m == 11) {
+            return 30;
+        }
+
+        return 31;
+    }
+
+    public static boolean exist2021(int m, int d) {
+        if(m <= 12 && d <= lastDay(m)) {
+            return true;
+        }
+            
         return false;
     }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
