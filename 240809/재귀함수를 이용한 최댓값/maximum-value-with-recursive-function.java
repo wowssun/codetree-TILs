6033e09ce7414dee798c165ecda0f0b07b7a9d8a
wowssun <119738419+wowssun@users.noreply.github.com>
2024-08-09 15:57:31 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 240809/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
+++ 240809/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
@@ -1,29 +1,27 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_N = 100;
+    
+    public static int[] arr = new int[MAX_N];
 
-    public static int maxNum (int [] arr) {
-        int maxNum = arr[0];
-
-        for (int i = 1; i < arr.length; i++) {
-            if (maxNum < arr[i]) {
-                maxNum = arr[i];
-            }
-        }
-        return maxNum;
+    public static int maxNum(int n) {
+       if(n == 0)
+            return arr[0];
+    
+        return Math.max(maxNum(n - 1), arr[n]);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         int n = sc.nextInt();
-        int [] arr = new int[n];
 
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        System.out.print(maxNum(arr));
+        System.out.print(maxNum(n - 1));
 
 
     }
