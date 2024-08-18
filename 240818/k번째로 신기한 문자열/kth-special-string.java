103731ddd4f754816502fa25b6541c39b60d9b7a
wowssun <119738419+wowssun@users.noreply.github.com>
2024-08-18 16:35:36 +0900

    Title: k번째로 신기한 문자열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240818/k번째로 신기한 문자열/kth-special-string.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        String pattern = sc.next();
+        String [] arr = new String[n];
+        String [] hasPattern = new String[n];
+        int idx = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next();
+        }
+
+        for (int i = 0; i < n; i++) {
+            boolean result = true;
+            for (int j = 0; j < pattern.length(); j++) {
+                if (arr[i].charAt(j) != pattern.charAt(j)) {
+                    result = false;
+                }
+            }
+            if (result) {
+                hasPattern[idx++] = arr[i];
+            }
+        }
+
+        Arrays.sort(hasPattern,0,idx);
+        
+        System.out.print(hasPattern[k-1]);
+        
+    }
+}
