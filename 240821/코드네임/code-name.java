4072c85c223149520a5cecb16fe1fc0e7a403631
wowssun <119738419+wowssun@users.noreply.github.com>
2024-08-21 17:51:32 +0900

    Title: 코드네임, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240821/코드네임/code-name.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class AgentInfo {
+    String codeName;
+    int score;
+
+    public AgentInfo(String codeName, int score) {
+        this.codeName = codeName;
+        this.score = score;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        AgentInfo [] agents = new AgentInfo[5];
+
+        for (int i = 0; i < 5; i++) {
+            String inputName = sc.next();
+            int inputScore = sc.nextInt();
+            agents[i] = new AgentInfo(inputName, inputScore);
+        }
+
+        int min = agents[0].score;
+        int minIdx = 0;
+        for (int i = 0; i < 5; i++) {
+           if (agents[i].score < min) {
+                minIdx = i;
+           }
+        }
+        System.out.print(agents[minIdx].codeName + " " + agents[minIdx].score);
+
+    }
+}
